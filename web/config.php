64d8a03a4afbf9742f4547ae2df76d7d03a795b6
bitWolfy <bondarchuk.kirill@gmail.com>
2013-01-28 13:43:11 -0800

    Invalid configuration - this should be the same as default

--- web/config.php
+++ web/config.php
@@ -1,13 +1,13 @@
 <?php
-    define('SETUP_COMPLETE', true);
+    define('SETUP_COMPLETE', false);
     
-    define("SERVER_NAME", "WolvenCraft Statistics");
+    define('SERVER_NAME', 'Server Statistics');
     
-    define("DB_SERVER"  , "localhost");
-    define("DB_USER"    , "wolvencr_ccuser");
-    define("DB_PASSWORD", "a98huigs7286");
-    define("DB_NAME"    , "wolvencr_stats");
-    define("DB_PORT"    , "3306");
+    define('DB_SERVER'  , 'localhost');
+    define('DB_USER'    , '');
+    define('DB_PASSWORD', '');
+    define('DB_NAME'    , '');
+    define('DB_PORT'    , 3306);
 
     define('CLOCK24', true); // true = 24 hours; false = 12 hours
 
