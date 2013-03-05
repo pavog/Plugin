503e3c7dccfab71bd0d4617d523de5ad6ee3a5eb
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-03-05 10:03:33 -0800

    Add more server information to track

--- src/com/wolvencraft/yasp/db/data/ServerStatistics.java
+++ src/com/wolvencraft/yasp/db/data/ServerStatistics.java
@@ -2,6 +2,8 @@ package com.wolvencraft.yasp.db.data;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
+
 import com.wolvencraft.yasp.StatsPlugin;
 import com.wolvencraft.yasp.db.QueryResult;
 import com.wolvencraft.yasp.db.QueryUtils;
@@ -18,8 +20,15 @@ public class ServerStatistics {
 		currentUptime = 0;
 		totalUptime = 0;
 		maxPlayersOnline = 0;
+		plugins = Bukkit.getServer().getPluginManager().getPlugins().length;
+		maxPlayersAllowed = Bukkit.getMaxPlayers();
+		bukkitVersion = Bukkit.getServer().getBukkitVersion();
+		serverIP = Bukkit.getServer().getIp();
+		serverPort = Bukkit.getServer().getPort();
+		serverMOTD = Bukkit.getServer().getMotd();
 		
 		fetchData();
+		pushStaticData();
 	}
 	
 	private long lastSyncTime;
@@ -30,6 +39,14 @@ public class ServerStatistics {
 	private long totalUptime;
 	private int maxPlayersOnline;
 	private long maxPlayersOnlineTime;
+	private int plugins;
+	private String bukkitVersion;
+	
+	private String serverIP;
+	private int serverPort;
+	private String serverMOTD;
+	
+	private int maxPlayersAllowed;
 	
 	public void fetchData() {
 		List<QueryResult> entries = QueryUtils.select(_ServerStatistics.TableName.toString(), new String[] {"*"});
@@ -49,16 +66,6 @@ public class ServerStatistics {
 		lastSyncTime = curTime;
 		QueryUtils.update(
 			_ServerStatistics.TableName.toString(),
-			"value", firstStartupTime + "",
-			new String[] {"key", "first_startup"}
-		);
-		QueryUtils.update(
-			_ServerStatistics.TableName.toString(),
-			"value", startupTime + "",
-			new String[] {"key", "last_startup"}
-		);
-		QueryUtils.update(
-			_ServerStatistics.TableName.toString(),
 			"value", currentUptime + "",
 			new String[] {"key", "current_uptime"}
 		);
@@ -73,11 +80,49 @@ public class ServerStatistics {
 			new String[] {"key", "max_players_online"}
 		);
 		QueryUtils.update(
-				_ServerStatistics.TableName.toString(),
-				"value", maxPlayersOnlineTime + "",
-				new String[] {"key", "max_players_online_time"}
-			);
+			_ServerStatistics.TableName.toString(),
+			"value", maxPlayersOnlineTime + "",
+			new String[] {"key", "max_players_online_time"}
+		);
 		return true;
+	}
+	
+	public void pushStaticData() {
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", firstStartupTime + "",
+			new String[] {"key", "first_startup"}
+		);
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", startupTime + "",
+			new String[] {"key", "last_startup"}
+		);
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", plugins + "",
+			new String[] {"key", "plugins"}
+		);
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", bukkitVersion,
+			new String[] {"key", "bukkit_version"}
+		);
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", serverIP,
+			new String[] {"key", "server_ip"}
+		);
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", serverPort + "",
+			new String[] {"key", "server_port"}
+		);
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", serverMOTD,
+			new String[] {"key", "server_motd"}
+		);
 	}
 	
 	/**
@@ -100,5 +145,17 @@ public class ServerStatistics {
 			this.maxPlayersOnline = playersOnline;
 			this.maxPlayersOnlineTime = Util.getTimestamp();
 		}
+		
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", maxPlayersAllowed + "",
+			new String[] {"key", "players_allowed"}
+		);
+		
+		QueryUtils.update(
+			_ServerStatistics.TableName.toString(),
+			"value", playersOnline + "",
+			new String[] {"key", "players_online"}
+		);
 	}
 }
