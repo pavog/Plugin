b3e5afc2802e9b90cb542685534d0aad4d41db9a
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-03-04 19:47:48 -0800

    Fix: Can only send messages to the console through the async task

--- src/com/wolvencraft/yasp/cmd/RepatchCommand.java
+++ src/com/wolvencraft/yasp/cmd/RepatchCommand.java
@@ -11,17 +11,17 @@ public class RepatchCommand implements BaseCommand {
 
 	@Override
 	public boolean run(String[] args) {
+		Message.sendFormattedSuccess(CommandManager.getSender(), "Attempting to patch the database...");
 		Bukkit.getScheduler().runTaskAsynchronously(StatsPlugin.getInstance(), new Runnable() {
 
 			@Override
 			public void run() {
-				Message.sendFormattedSuccess(CommandManager.getSender(), "Attempting to patch the database...");
 				try { Database.getInstance().patch(true); }
 				catch (Exception ex) { Message.sendFormattedError(CommandManager.getSender(), "Patch failed"); }
-				Message.sendFormattedSuccess(CommandManager.getSender(), "Patching complete");
 			}
 			
 		});
+		Message.sendFormattedSuccess(CommandManager.getSender(), "Patching complete");
 		return true;
 	}
 
