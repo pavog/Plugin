5c4b968be211539cf64149ece84b5b2ecd45e30a
Christian Swan <dazzel@online.de>
2013-03-12 19:52:17 +0100

    remove distance_player_id

--- SQLPatches/yasp_v1.sql
+++ SQLPatches/yasp_v1.sql
@@ -49,15 +49,14 @@ CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_detailed_log_players` (
 DROP TABLE IF EXISTS `$dbname`.`$prefix_distance_players` ;
 
 CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_distance_players` (
-  `distance_player_id` INT NOT NULL AUTO_INCREMENT ,
   `player_id` INT NOT NULL ,
   `foot` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 ,
-  `swimmed` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 ,
   `boat` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 ,
   `minecart` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 ,
   `pig` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 ,
-  PRIMARY KEY (`distance_player_id`) ,
+  `swimmed` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 ,
   INDEX `fk_player_id1_idx` (`player_id` ASC) ,
+  PRIMARY KEY (`player_id`) ,
   CONSTRAINT `fk_player_id1`
     FOREIGN KEY (`player_id` )
     REFERENCES `$dbname`.`$prefix_players` (`player_id` )
@@ -499,7 +498,6 @@ CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_server_statistics` (
 DROP TABLE IF EXISTS `$dbname`.`$prefix_misc_info_players` ;
 
 CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_misc_info_players` (
-  `misc_info_players_id` INT NOT NULL AUTO_INCREMENT ,
   `player_id` INT NOT NULL ,
   `gamemode` TINYINT(2) NOT NULL DEFAULT 0 ,
   `exp_level` SMALLINT UNSIGNED NOT NULL DEFAULT 0 ,
@@ -517,14 +515,12 @@ CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_misc_info_players` (
   `commands_sent` INT UNSIGNED NOT NULL DEFAULT 0 ,
   `beds_entered` INT UNSIGNED NOT NULL DEFAULT 0 ,
   `portals_entered` INT UNSIGNED NOT NULL DEFAULT 0 ,
-  PRIMARY KEY (`misc_info_players_id`) ,
-  INDEX `fk_player_id9_idx` (`player_id` ASC) ,
+  PRIMARY KEY (`player_id`) ,
   CONSTRAINT `fk_player_id9`
     FOREIGN KEY (`player_id` )
     REFERENCES `$dbname`.`$prefix_players` (`player_id` )
     ON DELETE CASCADE
-    ON UPDATE NO ACTION);
-
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 USE `$dbname` ;
