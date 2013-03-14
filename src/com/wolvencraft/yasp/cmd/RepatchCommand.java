aa1b7f5676716d07f8e5e8f3b0fc676078b2174b
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-03-13 18:15:21 -0700

    Re-instantiate player sessions after the patch is complete

--- src/com/wolvencraft/yasp/cmd/RepatchCommand.java
+++ src/com/wolvencraft/yasp/cmd/RepatchCommand.java
@@ -1,17 +1,21 @@
 package com.wolvencraft.yasp.cmd;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import com.wolvencraft.yasp.CommandManager;
+import com.wolvencraft.yasp.DataCollector;
 import com.wolvencraft.yasp.StatsPlugin;
 import com.wolvencraft.yasp.db.Database;
 import com.wolvencraft.yasp.util.Message;
+import com.wolvencraft.yasp.util.Util;
 
 public class RepatchCommand implements BaseCommand {
 
 	@Override
 	public boolean run(String[] args) {
 		Message.sendFormattedSuccess(CommandManager.getSender(), "Attempting to patch the database...");
+		DataCollector.dumpAll();
 		StatsPlugin.setPaused(true);
 		Bukkit.getScheduler().runTaskAsynchronously(StatsPlugin.getInstance(), new Runnable() {
 
@@ -19,7 +23,11 @@ public class RepatchCommand implements BaseCommand {
 			public void run() {
 				try { Database.getInstance().runSyncPatch(true); }
 				catch (Exception ex) { Message.sendFormattedError(CommandManager.getSender(), "Patch failed"); }
-				finally { Message.sendFormattedSuccess(CommandManager.getSender(), "Patching complete"); StatsPlugin.setPaused(false); }
+				finally {
+					for(Player player : Bukkit.getServer().getOnlinePlayers()) {
+						if(!Util.isExempt(player)) DataCollector.get(player);
+					}
+				}
 			}
 			
 		});
