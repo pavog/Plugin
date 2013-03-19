1322dddd6b17ee5169bd2b61b9869e03aff8afc6
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-03-19 09:32:32 -0700

    Incorrect copyright notice

--- src/com/wolvencraft/yasp/cmd/HelpCommand.java
+++ src/com/wolvencraft/yasp/cmd/HelpCommand.java
@@ -1,27 +1,20 @@
-/* 
- *    Copyright 2009-2011 The MyBatis Team
+/*
+ * Statistics
+ * Copyright (C) 2013 bitWolfy <http://www.wolvencraft.com> and contributors
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *    
- *    Changelog:
- *    
- *    Cut down on unused code and generally optimized for desired tasks.
- *    - bitWolfy
- *    
- *    Added the ability to change the delimiter so you can run scripts that 
- *    contain stored procedures.
- *    - ChaseHQ
- */
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package com.wolvencraft.yasp.cmd;
 
