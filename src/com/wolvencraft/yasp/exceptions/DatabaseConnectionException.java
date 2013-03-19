f09ccb59150010f7d5b9c837d5c717d2d2080bc5
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-03-19 08:55:59 -0700

    Document exceptions properly

--- src/com/wolvencraft/yasp/exceptions/DatabaseConnectionException.java
+++ src/com/wolvencraft/yasp/exceptions/DatabaseConnectionException.java
@@ -1,5 +1,28 @@
+/*
+ * Statistics
+ * Copyright (C) 2013 bitWolfy <http://www.wolvencraft.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.wolvencraft.yasp.exceptions;
 
+/**
+ * Exception thrown when an error occurs while connecting to the database
+ * @author bitWolfy
+ *
+ */
 public class DatabaseConnectionException extends Exception {
 	private static final long serialVersionUID = 1325478012737090439L;
 
