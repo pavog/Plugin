1ed56e2c816763c64bb0940ddd7da348713828f1
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-03-19 08:54:24 -0700

    Document event listeners properly

--- src/com/wolvencraft/yasp/listeners/DeathListener.java
+++ src/com/wolvencraft/yasp/listeners/DeathListener.java
@@ -1,3 +1,21 @@
+/*
+ * Statistics
+ * Copyright (C) 2013 bitWolfy <http://www.wolvencraft.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.wolvencraft.yasp.listeners;
 
 import org.bukkit.Material;
@@ -21,8 +39,18 @@ import com.wolvencraft.yasp.DataCollector;
 import com.wolvencraft.yasp.StatsPlugin;
 import com.wolvencraft.yasp.util.Util;
 
+/**
+ * Listens to any entity deaths on the server and reports them to the plugin
+ * @author bitWolfy
+ *
+ */
 public class DeathListener implements Listener {
-
+	
+	/**
+	 * <b>Default constructor</b><br />
+	 * Creates a new instance of the Listener and registers it with the PluginManager
+	 * @param plugin StatsPlugin instance
+	 */
 	public DeathListener(StatsPlugin plugin) {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
--- src/com/wolvencraft/yasp/listeners/ServerListener.java
+++ src/com/wolvencraft/yasp/listeners/ServerListener.java
@@ -1,3 +1,21 @@
+/*
+ * Statistics
+ * Copyright (C) 2013 bitWolfy <http://www.wolvencraft.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.wolvencraft.yasp.listeners;
 
 import org.bukkit.Bukkit;
@@ -11,8 +29,18 @@ import org.bukkit.event.weather.WeatherChangeEvent;
 import com.wolvencraft.yasp.DataCollector;
 import com.wolvencraft.yasp.StatsPlugin;
 
+/**
+ * Listens to generic server events and reports them to the plugin.
+ * @author bitWolfy
+ *
+ */
 public class ServerListener implements Listener {
 	
+	/**
+	 * <b>Default constructor</b><br />
+	 * Creates a new instance of the Listener and registers it with the PluginManager
+	 * @param plugin StatsPlugin instance
+	 */
 	public ServerListener(StatsPlugin plugin) {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
