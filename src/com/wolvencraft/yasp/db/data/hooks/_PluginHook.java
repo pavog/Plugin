d5d3f70e0c8fb8d6a3d131a1f58ee9074d910047
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-03-19 09:15:25 -0700

    Document the data stores properly (again)

--- src/com/wolvencraft/yasp/db/data/hooks/_PluginHook.java
+++ src/com/wolvencraft/yasp/db/data/hooks/_PluginHook.java
@@ -1,9 +1,32 @@
+/*
+ * Statistics
+ * Copyright (C) 2013 bitWolfy <http://www.wolvencraft.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.wolvencraft.yasp.db.data.hooks;
 
 import java.util.Map;
 
 import org.bukkit.entity.Player;
 
+/**
+ * Common interface for plugin hook data stores
+ * @author bitWolfy
+ *
+ */
 public interface _PluginHook {
 	
 	/**
