1ef4b59ba55567a798c6f88037fba7e5fc7c3c2c
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-04-16 10:40:21 -0700

    More fixes towards the hook patchfiles

--- patches/1.wg.sql
+++ patches/1.wg.sql
@@ -1,3 +1,12 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+SET storage_engine=InnoDB;
+
+CREATE SCHEMA IF NOT EXISTS `$dbname` DEFAULT CHARACTER SET utf8 ;
+USE `$dbname` ;
+
 -- -----------------------------------------------------
 -- Table `$dbname`.`$prefix_wg_regions`
 -- -----------------------------------------------------
@@ -12,5 +21,8 @@ CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_wg_regions` (
     FOREIGN KEY (`player_id` )
     REFERENCES `$dbname`.`$prefix_players` (`player_id` )
     ON DELETE CASCADE
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
+    
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
