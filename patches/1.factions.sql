e935cdda5d002965163bfbd94bec19aeb8ab96d3
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-04-16 10:37:53 -0700

    Fixes towards the hook patch files.

--- patches/1.factions.sql
+++ patches/1.factions.sql
@@ -2,13 +2,12 @@
 -- Table `$dbname`.`$prefix_factions`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `$dbname`.`$prefix_factions` ;
+
 CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_factions` (
-  `faction_id` INT NOT NULL ,
   `player_id` INT NOT NULL ,
   `faction` VARCHAR(255) NOT NULL DEFAULT 'none' ,
   `current_position` VARCHAR(255) NULL ,
-  PRIMARY KEY (`faction_id`) ,
-  INDEX `fk_player_id22_idx` (`player_id` ASC) ,
+  PRIMARY KEY (`player_id`) ,
   CONSTRAINT `fk_player_id22`
     FOREIGN KEY (`player_id` )
     REFERENCES `$dbname`.`$prefix_players` (`player_id` )
--- patches/1.vault.sql
+++ patches/1.vault.sql
@@ -2,13 +2,12 @@
 -- Table `$dbname`.`$prefix_vaults`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `$dbname`.`$prefix_vaults` ;
+
 CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_vaults` (
-  `vault_id` INT NOT NULL AUTO_INCREMENT ,
   `player_id` INT NOT NULL ,
-  `group` VARCHAR(255) NOT NULL DEFAULT 'none' ,
+  `group` TEXT NOT NULL DEFAULT '' ,
   `balance` FLOAT NOT NULL DEFAULT 0.0 ,
-  PRIMARY KEY (`vault_id`) ,
-  INDEX `fk_player_id20_idx` (`player_id` ASC) ,
+  PRIMARY KEY (`player_id`) ,
   CONSTRAINT `fk_player_id20`
     FOREIGN KEY (`player_id` )
     REFERENCES `$dbname`.`$prefix_players` (`player_id` )
