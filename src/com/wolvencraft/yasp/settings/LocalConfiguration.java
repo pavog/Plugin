7014d0ae3383b1f6f30c621274e28646f0ea1658
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-04-23 13:02:41 -0700

    Fix: Add missing classes back into the git structure

---
+++ src/com/wolvencraft/yasp/settings/LocalConfiguration.java
@@ -0,0 +1,102 @@
+/*
+ * LocalConfiguration.java
+ * 
+ * Statistics
+ * Copyright (C) 2013 bitWolfy <http://www.wolvencraft.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.wolvencraft.yasp.settings;
+
+import com.wolvencraft.yasp.Statistics;
+
+
+/**
+ * Represents the local configuration, stored in <i>config.yml</i>
+ * @author bitWolfy
+ *
+ */
+public enum LocalConfiguration {
+    Debug           ("debug", true),
+    DBHost          ("database.host", true),
+    DBPort          ("database.port", true),
+    DBName          ("database.name", true),
+    DBUser          ("database.user", true),
+    DBPass          ("database.pass", true),
+    DBPrefix        ("database.prefix", true),
+    DBConnect       ("jdbc:mysql://" + DBHost.asString() + ":" + DBPort.asInteger() + "/" + DBName.asString()),
+    LogPrefix       ("log-prefix", true),
+    FetchPatches    ("fetch-patches", true),
+    PatchServer     ("http://dl.wolvencraft.com/raw/Statistics/"),
+    Standalone      (true);
+    
+    Object entry;
+    
+    /**
+     * <b>Default constructor</b><br />
+     * Creates a new LocalConfiguration with the specified value
+     * @param entry Configuration value
+     */
+    LocalConfiguration(Object entry) {
+        this.entry = entry;
+    }
+    
+    /**
+     * <b>Constructor</b><br />
+     * Creates a new LocalConfiguration entry with the value that can be pulled from <code>config.yml</code>.
+     * @param entry Configuration value or <code>config.yml</code> node
+     * @param fromFile If <b>true</b>, configuration values will be pulled from file, otherwise, the <code>entry</code> will be used
+     */
+    LocalConfiguration(Object entry, boolean fromFile) {
+        if(fromFile) this.entry = Statistics.getInstance().getConfig().get((String) entry);
+        else this.entry = entry;
+    }
+    
+    /**
+     * Returns the configuration value as String
+     * @deprecated <code>asString();</code> should be used instead
+     * @return Configuration value
+     */
+    @Override
+    public String toString() {
+        return asString();
+    }
+    
+    /**
+     * Returns the configuration value as String
+     * @return Configuration value
+     */
+    public String asString() {
+        return (String) entry;
+    }
+    
+    /**
+     * Returns the configuration value as a boolean
+     * @return Configuration value
+     */
+    public boolean asBoolean() {
+        try { return (Boolean) entry; }
+        catch (Throwable t) { return false; }
+    }
+    
+    /**
+     * Returns the configuration value as an integer
+     * @return Configuration value
+     */
+    public int asInteger() {
+        try { return (Integer) entry; }
+        catch (Throwable t) { return 0; }
+    }
+}
---
+++ src/com/wolvencraft/yasp/settings/Module.java
@@ -0,0 +1,144 @@
+/*
+ * Module.java
+ * 
+ * Statistics
+ * Copyright (C) 2013 bitWolfy <http://www.wolvencraft.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.wolvencraft.yasp.settings;
+
+import com.wolvencraft.yasp.db.Query;
+import com.wolvencraft.yasp.db.tables.Normal.SettingsTable;
+
+/**
+ * Represents the different plugin modules
+ * @author bitWolfy
+ *
+ */
+public enum Module {
+    
+    Server("server"),
+    Blocks("blocks"),
+    Items("items"),
+    Deaths("deaths"),
+    Inventory("inventory"),
+    
+    Vault("vault", true),
+    McMMO("mcmmo", true),
+    Jobs("jobs", true),
+    WorldGuard("worldguard", true),
+    MobArena("mobarena", true),
+    PvpArena("pvparena", true),
+    Factions("factions", true);
+    
+    private boolean isHook;
+    private String key;
+    
+    private boolean refreshScheduled;
+    
+    private boolean enabled;
+    private boolean active;
+    
+    private int version;
+    
+    Module(String key) {
+        this.isHook = false;
+        this.key = key;
+        
+        refresh();
+        
+        refreshScheduled = false;
+    }
+    
+    Module(String key, boolean isHook) {
+        this.isHook = isHook;
+        this.key = key;
+        
+        refresh();
+        
+        refreshScheduled = false;
+    }
+
+    /**
+     * Returns the state of the module specified in the configuration.
+     * @return <b>true</b> if the module is enabled, <b>false</b> if it is not
+     */
+    public boolean isEnabled() {
+        if(refreshScheduled) refresh();
+        return enabled;
+    }
+
+    /**
+     * Checks if the module is actually active.
+     * @return <b>true</b> if the module is both enabled and active, <b>false</b> otherwise
+     */
+    public boolean isActive() {
+        return enabled && active;
+    }
+    
+    /**
+     * Sets the active status of the module.
+     * 
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * Returns the version of the module.<br />
+     * Only relevant if the module is a plugin hook.
+     * @return Module version
+     */
+    public int getVersion() {
+        if(refreshScheduled) refresh();
+        return version;
+    }
+    
+    private void refresh() {
+        String stateKey = "";
+        if(isHook) {
+            stateKey = "hook." + key;
+            
+            String versionKey = "version." + key;
+            if(Query.table(SettingsTable.TableName).condition("key", versionKey).exists()) {
+                try { version = Query.table(SettingsTable.TableName).column("value").condition("key", versionKey).select().asInt("value"); }
+                catch (Throwable t) { enabled = true; }
+            } else {
+                Query.table(SettingsTable.TableName).value("key", versionKey).value("value", 0).insert();
+                enabled = true;
+            }
+        } else {
+            stateKey = "module." + key;
+            version = -1;
+        }
+        
+        if(Query.table(SettingsTable.TableName).condition("key", stateKey).exists()) {
+            try { enabled = Query.table(SettingsTable.TableName).column("value").condition("key", stateKey).select().asBoolean("value"); }
+            catch (Throwable t) { enabled = true; }
+        } else {
+            Query.table(SettingsTable.TableName).value("key", stateKey).value("value", true).insert();
+            enabled = true;
+        }
+    }
+    
+    /**
+     * Schedules all the modules to refresh
+     */
+    public static void clearCache() {
+        for(Module module : Module.values()) module.refreshScheduled = true;
+    }
+    
+}
