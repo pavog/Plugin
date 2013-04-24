7fd85c496f8a0e57b077b8c1621cfbfd7e3b7a32
Christian Swan <dazzel@online.de>
2013-04-24 18:52:32 +0300

    add some more columns

--- patches/1.factions.sql
+++ patches/1.factions.sql
@@ -16,6 +16,10 @@ CREATE  TABLE IF NOT EXISTS `$dbname`.`$prefix_factions` (
   `player_id` INT NOT NULL ,
   `faction` VARCHAR(255) NOT NULL DEFAULT 'none' ,
   `current_position` VARCHAR(255) NULL ,
+  `current_power` FLOAT NOT NULL DEFAULT 0.0 ,
+  `max_power` FLOAT NOT NULL DEFAULT 0.0 ,
+  `role` VARCHAR(255) NULL ,
+  `title` VARCHAR(25) NULL ,
   PRIMARY KEY (`player_id`) ,
   CONSTRAINT `fk_player_id22`
     FOREIGN KEY (`player_id` )
