a664e93f9152309a42e61d053ac0a7fb9205dc3e
MarioG1 <Mario@Mario-PC.fritz.box>
2014-01-21 19:34:39 +0100

    Added all new 1.7 Items

---
+++ patches/8.yaspx.sql
@@ -0,0 +1,72 @@
+-- Materials for 1.7
+-- Flowers 
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:0", "poppy") ON DUPLICATE KEY UPDATE `tp_name` = "poppy";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:1", "blue_orchid") ON DUPLICATE KEY UPDATE `tp_name` = "blue_orchid";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:2", "allium")  ON DUPLICATE KEY UPDATE `tp_name` = "allium";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:3", "azure_bluet")  ON DUPLICATE KEY UPDATE `tp_name` = "azure_bluet";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:4", "tulip_red")  ON DUPLICATE KEY UPDATE `tp_name` = "tulip_red";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:5", "tulip_orange") ON DUPLICATE KEY UPDATE `tp_name` = "tulip_orange";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:6", "tulip_white") ON DUPLICATE KEY UPDATE `tp_name` = "tulip_white";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:7", "tulip_pink") ON DUPLICATE KEY UPDATE `tp_name` = "tulip_pink";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("38:8", "oxye_daisy") ON DUPLICATE KEY UPDATE `tp_name` = "oxye_daisy";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("175:0", "sunflower") ON DUPLICATE KEY UPDATE `tp_name` = "sunflower";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("175:1", "lilac") ON DUPLICATE KEY UPDATE `tp_name` = "lilac";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("175:2", "double_tall_grass") ON DUPLICATE KEY UPDATE `tp_name` = "double_tall_grass";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("175:3", "tall_fern") ON DUPLICATE KEY UPDATE `tp_name` = "tall_fern";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("175:4", "rose_bush") ON DUPLICATE KEY UPDATE `tp_name` = "rose_bush";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("175:5", "peony") ON DUPLICATE KEY UPDATE `tp_name` = "peony";
+-- Glass Panes
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:0", "glass_pane_white") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_white";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:1", "glass_pane_orange") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_orange";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:2", "glass_pane_magenta") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_magenta";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:3", "glass_pane_light_blue") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_light_blue";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:4", "glass_pane_yellow") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_yellow";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:5", "glass_pane_lime") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_lime";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:6", "glass_pane_pink") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_pink";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:7", "glass_pane_gray") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_gray";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:8", "glass_pane_light_gray") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_light_gray";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:9", "glass_pane_cyan") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_cyan";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:10", "glass_pane_purple") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_purple";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:11", "glass_pane_blue") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_blue";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:12", "glass_pane_brown") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_brown";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:13", "glass_pane_green") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_green";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:14", "glass_pane_red") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_red";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("160:15", "glass_pane_black") ON DUPLICATE KEY UPDATE `tp_name` = "glass_pane_black";
+-- Glass Blocks
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:0", "glass_stained_white") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_white";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:1", "glass_stained_orange") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_orange";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:2", "glass_stained_magenta") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_magenta";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:3", "glass_stained_light_blue") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_light_blue";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:4", "glass_stained_yellow") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_yellow";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:5", "glass_stained_lime") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_lime";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:6", "glass_stained_pink") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_pink";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:7", "glass_stained_gray") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_gray";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:8", "glass_stained_light_gray") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_light_gray";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:9", "glass_stained_cyan") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_cyan";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:10", "glass_stained_purple") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_purple";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:11", "glass_stained_blue") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_blue";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:12", "glass_stained_brown") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_brown";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:13", "glass_stained_green") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_green";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:14", "glass_stained_red") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_red";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("95:15", "glass_stained_black") ON DUPLICATE KEY UPDATE `tp_name` = "glass_stained_black";
+-- Fish
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("349:1", "raw_salmon") ON DUPLICATE KEY UPDATE `tp_name` = "raw_salmon";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("349:2", "clownfish") ON DUPLICATE KEY UPDATE `tp_name` = "clownfish";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("349:3", "pufferfish") ON DUPLICATE KEY UPDATE `tp_name` = "pufferfish";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("350:1", "cooked_salmon") ON DUPLICATE KEY UPDATE `tp_name` = "cooked_salmon";
+-- Wood
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("5:4", "wooden_plank_acacia") ON DUPLICATE KEY UPDATE `tp_name` = "wooden_plank_acacia";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("5:5", "wooden_plank_dark_oak") ON DUPLICATE KEY UPDATE `tp_name` = "wooden_plank_dark_oak";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("6:4", "sapling_acacia") ON DUPLICATE KEY UPDATE `tp_name` = "sapling_acacia";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("6:5", "sapling_dark_oak") ON DUPLICATE KEY UPDATE `tp_name` = "sapling_dark_oak";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("126:5", "dark_oak-wood_slab") ON DUPLICATE KEY UPDATE `tp_name` = "dark_oak-wood_slab";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("126:4", "acacia-wood_slab") ON DUPLICATE KEY UPDATE `tp_name` = "acacia-wood_slab";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("161:0", "leaves_acacia") ON DUPLICATE KEY UPDATE `tp_name` = "leaves_acacia";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("161:1", "leaves_dark_oak") ON DUPLICATE KEY UPDATE `tp_name` = "leaves_dark_oak";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("162:0", "wood_acacia") ON DUPLICATE KEY UPDATE `tp_name` = "wood_acacia";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("162:1", "wood_dark_oak") ON DUPLICATE KEY UPDATE `tp_name` = "wood_dark_oak";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("163:0", "wooden_stairs_acacia") ON DUPLICATE KEY UPDATE `tp_name` = "wooden_stairs_acacia";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("164:0", "wooden_stairs_dark_oak") ON DUPLICATE KEY UPDATE `tp_name` = "wooden_stairs_dark_oak";
+-- Other Blocks
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("3:2", "podzol") ON DUPLICATE KEY UPDATE `tp_name` = "podzol";
+INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("12:1", "sand_red") ON DUPLICATE KEY UPDATE `tp_name` = "sand_red";
