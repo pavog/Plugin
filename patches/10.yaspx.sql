d7e0b8ce7e824142bf1866610f18834ce5102c3a
MarioG1 <mario.gallaun@gmx.at>
2014-02-03 20:11:26 +0100

    Normal horses have 100 as entity id not 100:0

--- patches/10.yaspx.sql
+++ patches/10.yaspx.sql
@@ -1,2 +1,3 @@
 -- Fix normal horses 100
+DELETE FROM `$prefix_etities` WHERE `etities_id` IS "100:0";
 INSERT INTO `$prefix_etities` (`etities_id`, `tp_name`) VALUES ("100", "horse") ON DUPLICATE KEY UPDATE `tp_name` = "horse";
