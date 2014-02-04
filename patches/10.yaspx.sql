c739590efb44ca816d7f61a775548eb905b3356c
MarioG1 <mario.gallaun@gmx.at>
2014-02-04 19:32:35 +0100

    Fixed patch :)

--- patches/10.yaspx.sql
+++ patches/10.yaspx.sql
@@ -1,6 +1,6 @@
 -- Fix normal horses 100
-DELETE FROM `$prefix_etities` WHERE `etities_id` IS "100:0";
-INSERT INTO `$prefix_etities` (`etities_id`, `tp_name`) VALUES ("100", "horse") ON DUPLICATE KEY UPDATE `tp_name` = "horse";
+DELETE FROM `$prefix_entities` WHERE `entity_id` = "100:0";
+INSERT INTO `$prefix_entities` (`entity_id`, `tp_name`) VALUES ("100", "horse") ON DUPLICATE KEY UPDATE `tp_name` = "horse";
 
 -- Add missing grassless dirt
 INSERT INTO `$prefix_materials` (`material_id`, `tp_name`) VALUES ("3:1", "grassless_dirt") ON DUPLICATE KEY UPDATE `tp_name` = "grassless_dirt";
